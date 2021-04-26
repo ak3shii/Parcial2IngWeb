c6d885417fd68fbd00f8bf49f8b0f8b02116c526
Jonathan Rodriguez <jonadrod999@hotmail.com>
2021-04-26 18:50:25 -0500

    Cambios menu

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,12 @@
 // var mostrar = false;
 
 let btnmenu = document.getElementById('btnmenu'); 
-let menu = document.getElementById('menu'); 
+let menu = document.getElementById('menu1'); 
 
 btnmenu.addEventListener('click', function(){
    'use strict';
     menu.classList.toggle('mostrar');
-    
+    console.log('Holaaaaa');
 });
 
 // function mostrarPC(){
